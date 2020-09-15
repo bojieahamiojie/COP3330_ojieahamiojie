3c1c16684afc8661208b89d90dcbb853d5a1ef9f
Geo Ojie <bojieahamiojie@knights.ucf.edu>
2020-09-14 20:57:15 -0400

    Last minute changes

--- ojieahamiojie_p1/src/Decrypter.java
+++ ojieahamiojie_p1/src/Decrypter.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Decrypter {
 
-    public void main(String[] args){
+    public void decrypt(String[] args){
         System.out.println("Enter the encrypted integers: \n");
         Scanner sc = new Scanner(System.in);
 
--- ojieahamiojie_p1/src/Encrypter.java
+++ ojieahamiojie_p1/src/Encrypter.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Encrypter {
 
-    public void main(String[] args){
+    public void encrypt(String[] args){
         System.out.println("Enter 4 integers: \n");
         Scanner sc = new Scanner(System.in);
 
