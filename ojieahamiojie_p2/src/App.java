a90416cf99ec65a0133252dbe9b136fa97114a6d
Geo Ojie <bojieahamiojie@knights.ucf.edu>
2020-09-21 18:00:15 -0400

    Updates

--- ojieahamiojie_p2/src/App.java
+++ ojieahamiojie_p2/src/App.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class App {
+
+    public static void main(String[] args){
+        ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
+
+        while(moreInput()){
+            double height = getUserheight();
+            double weight = getUserWeight();
+
+            BodyMassIndex bmi = new BodyMassIndex(height, weight);
+            bmiData.add(bmi);
+
+            displayBmiInfo(bmi);
+        }
+
+        displayBmiStatistics(bmiData);
+    }
 }
