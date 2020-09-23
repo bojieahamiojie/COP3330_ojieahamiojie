61c9f57c8a5485d911c6618c8e65e12d9836f310
Geo Ojie <bojieahamiojie@knights.ucf.edu>
2020-09-23 16:58:51 -0400

    had to erase bmi class

--- ojieahamiojie_p2/src/BodyMassIndex.java
+++ ojieahamiojie_p2/src/BodyMassIndex.java
@@ -1,15 +1,5 @@
 import java.util.Scanner;
 
 public class BodyMassIndex {
-
-    public static double bmiData() {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter your weight in pounds: \n");
-        double weight = input.nextDouble();
-        System.out.println("Enter your height using inches: \n");
-        double height = input.nextDouble();
-
-        
-    }
+    
 }
