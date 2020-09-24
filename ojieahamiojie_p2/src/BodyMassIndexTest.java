7f91dd7cb9ec6788855e99ed05b317d0ae15d695
Geo Ojie <bojieahamiojie@knights.ucf.edu>
2020-09-24 18:31:44 -0400

    Testing public methods...

--- ojieahamiojie_p2/src/BodyMassIndexTest.java
+++ ojieahamiojie_p2/src/BodyMassIndexTest.java
@@ -3,26 +3,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BodyMassIndexTest {
     @Test
-    public void Testbmi1(){
+    public void testBMI1(){
         BodyMassIndex bmi = new BodyMassIndex(69, 129);
         assertEquals(19.0, bmi.getBMI());
     }
 
     @Test
-    public void Testbmi2(){
+    public void testBMI2(){
         BodyMassIndex bmi = new BodyMassIndex(64, 126);
         assertEquals(21.6, bmi.getBMI());
     }
 
     @Test
-    public void Testbmi3(){
+    public void testBMI3(){
         BodyMassIndex bmi = new BodyMassIndex(72, 187);
         assertEquals(25.4, bmi.getBMI());
     }
 
     @Test
-    public void Testbmi4(){
+    public void testBMI4(){
         BodyMassIndex bmi = new BodyMassIndex(60, 159);
         assertEquals(31.0, bmi.getBMI());
+    }
+
+    @Test
+    public void testBodyMassIndex(){
+    }
+
+    @Test
+    public void testgetBMI(){
+    }
+
+    @Test
+    public void testcalculateBMI(){
+    }
+
+    @Test
+    public void testBMICategory(){
+    }
+
+    @Test
+    public void testgetBMICategory(){
+    }
+
+    @Test
+    public void testtoString(){
     }
 }
